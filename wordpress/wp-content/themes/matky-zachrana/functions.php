0fb78fb5533d1e1db0c1f19ff5f4b7236fb7f17a
Šimon Siksta <simon.siksta@student.ossp.cz>
2022-03-04 13:54:42 +0100

    cool

--- wordpress/wp-content/themes/matky-zachrana/functions.php
+++ wordpress/wp-content/themes/matky-zachrana/functions.php
@@ -1,1 +1,16 @@
 <?php
+add_action('wp_enqueue_scripts', 'enqueue_parents_css', 10);
+function enqueue_parents_css()
+{
+    wp_enqueue_style(
+        'parent-style',
+        trailingslashit(get_template_directory_uri()) . 'style.css',
+        array()
+    );
+
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_directory_uri() . '/style.css' ,
+        array('parent-style')
+    );
+}
