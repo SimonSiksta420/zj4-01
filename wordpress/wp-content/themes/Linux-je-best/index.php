05c88141229a07d27f2d9ee8518a160b128a0b66
Šimon Siksta <simon.siksta@student.ossp.cz>
2022-04-29 13:02:34 +0200

    Gayming

--- wordpress/wp-content/themes/Linux-je-best/index.php
+++ wordpress/wp-content/themes/Linux-je-best/index.php
@@ -1,30 +1,37 @@
-<?php get_header() ;?>
+<?php get_header(); ?>
 
 <?php
 
 if (have_posts()) :
 
-while (have_posts()) : the_post(); 
-    
+    while (have_posts()) : the_post();
+
 ?>
 
-<h1> <?php the_title() ;?> </h1>
+        
 
-<?php the_author() ;?>
+        <h2 id="post-<?php the_ID(); ?>">
+            <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
+                <?php the_title(); ?>
+            </a>
+        </h2>
 
-<?php the_date() ;?>
+        
+        <?php the_author(); ?>
+        
+        <?php the_date(); ?>
+        
+        <p> <?php the_post_thumbnail('small'); ?> </p>
+        
+    <?php
 
-<?php the_content() ;?>
-
-<?php
-
-endwhile;
+    endwhile;
 
 else :
 
-?>
+    ?>
 
-<p>Žádné příspěvky nalezeny.</p>
+    <p>Žádné příspěvky nalezeny.</p>
 
 <?php
 
@@ -32,4 +39,4 @@ endif;
 
 ?>
 
-<?php get_footer() ;?>
+<?php get_footer(); ?>
